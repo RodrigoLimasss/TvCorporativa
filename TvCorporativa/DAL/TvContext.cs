5b2e2a08d994559ebc0ff467ec27398b412f953d
RodrigoLimasss <rodrigolimasss@gmail.com>
2014-09-16 23:05:59 -0300

    Mapeamento do Model PlayList

--- TvCorporativa/DAL/TvContext.cs
+++ TvCorporativa/DAL/TvContext.cs
@@ -15,6 +15,7 @@ namespace TvCorporativa.DAL
         public DbSet<Empresa> Empresas { get; set; }
         public DbSet<Feed> Feeds { get; set; }
         public DbSet<Midia> Midias { get; set; }
+        public DbSet<PlayList> PlayList { get; set; }
         public DbSet<Ponto> Pontos { get; set; }
         public DbSet<Template> Templates { get; set; }
         public DbSet<Usuario> Usuarios { get; set; }
